7f863d95e9312b5f4c3d2921d8f437834ff4305d
callsamu <samuelbree@gmail.com>
2026-04-08 22:00:43 -0300

    feat: use reactive state for notes

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,41 +6,31 @@ import { MarkdownEditor } from './editor/markdown-editor/markdown-editor';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { EditorService } from './editor/editor-service';
 import { Note } from './note';
+import { NoteService } from './note-service';
+import { SafeHTMLPipe } from './safe-html-pipe';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MarkdownEditor, ReactiveFormsModule],
+  imports: [RouterOutlet, MarkdownEditor, ReactiveFormsModule,SafeHTMLPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   counter = 2;
-  editor: EditorService = inject(EditorService);
+
+  editorService: EditorService = inject(EditorService);
+  noteService: NoteService = inject(NoteService);
 
   noteTitleControl = new FormControl('título');
-  notes: WritableSignal<Pick<Note, 'id' | 'title' | 'content'>[]> = signal([
-    {
-      id: 0,
-      title: 'foo',
-      content: '<p>lorem ipsum dolor</p>',
-    },
-    {
-      id: 1,
-      title: 'bar',
-      content: '<p>lorem <b>ipsum</b> dolor</p>',
-    }
-  ]);
-
+  notes: WritableSignal<Note[]> = signal(this.noteService.getAll())
 
   onCreate() {
-    this.notes.set([
-      ...this.notes(),
-      {
-        id: this.counter++,
-        title: this.noteTitleControl.value ?? '',
-        content: this.editor.getHTMLContent() ?? '',
-      }
-    ])
+    const note = this.noteService.create(
+      this.noteTitleControl.getRawValue() ?? '',
+      this.editorService.getHTMLContent() ?? '',
+    );
+
+    this.notes.set([...this.notes(), note]);
   }
 }
