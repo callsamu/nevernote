f0201e459f86694e71bbabff792d69c03cd106b8
callsamu <samuelbree@gmail.com>
2026-04-18 16:42:05 -0300

    feat: use new editor component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,35 @@
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { MarkdownEditor } from './editor/markdown-editor/markdown-editor';
-
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { EditorService } from './editor/editor-service';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Note } from './note';
 import { NoteService } from './note-service';
 import { SafeHTMLPipe } from './safe-html-pipe';
+import { NoteView } from './editor/note-view/note-view'
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MarkdownEditor, ReactiveFormsModule,SafeHTMLPipe],
+  imports: [RouterOutlet, ReactiveFormsModule, SafeHTMLPipe, NoteView],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   counter = 2;
-
-  editorService: EditorService = inject(EditorService);
   noteService: NoteService = inject(NoteService);
 
-  noteTitleControl = new FormControl('título');
   notes: WritableSignal<Note[]> = signal(this.noteService.getAll())
 
-  onCreate() {
-    const note = this.noteService.create(
-      this.noteTitleControl.getRawValue() ?? '',
-      this.editorService.getHTMLContent() ?? '',
-    );
+  editorOpen = signal(false);
+  selectedNote = signal<Note | null>(null);
 
-    this.notes.set([...this.notes(), note]);
+  select(note: Note) {
+    this.editorOpen.set(true);
+    this.selectedNote.set(note);
+  }
+
+  create() {
+    this.editorOpen.set(true)
+    this.selectedNote.set(null);
   }
 }
