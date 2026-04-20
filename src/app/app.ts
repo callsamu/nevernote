40e91de1e7e5c538c4f649b19a62e098ff51b9e9
callsamu <samuelbree@gmail.com>
2026-04-19 22:06:33 -0300

    feat: use note repository on main

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,23 +3,31 @@ import { RouterOutlet } from '@angular/router';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { Note } from './note';
-import { NoteService } from './note-service';
 import { SafeHTMLPipe } from './safe-html-pipe';
 import { NoteDraft, NoteView } from './editor/note-view/note-view'
 import { DatePipe } from '@angular/common';
+import { IDBNoteRepository } from './persistence/idb/idb-note-repository';
+import { NoteRepository } from './persistence/note-repository';
 
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ReactiveFormsModule, SafeHTMLPipe, NoteView, DatePipe],
+  providers: [{ provide: NoteRepository, useClass: IDBNoteRepository }],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  counter = 2;
-  noteService: NoteService = inject(NoteService);
+  repository = inject(NoteRepository);
 
-  notes: WritableSignal<Note[]> = signal(this.noteService.getAll())
+  notes: WritableSignal<Note[]> = signal([])
+
+  ngOnInit() {
+    const $result = this.repository.list({ sort: "title" });
+    $result.subscribe(({ items }) => {
+      this.notes.set(items);
+    });
+  }
 
   editorOpen = signal(false);
   selectedNote = signal<Note | null>(null);
@@ -35,12 +43,15 @@ export class App {
   }
 
   onNoteSaved(draft: NoteDraft) {
-    const note = this.noteService.create(
-      draft.title,
-      draft.content,
-    );
-    this.selectedNote.set(note);
-    this.notes.set([...this.notes(), note]);
-    console.log(note);
+    const note$ = this.repository.create({
+      title: draft.title,
+      content: draft.content,
+      notebookId: '',
+    });
+
+    note$.subscribe(note => {
+      this.selectedNote.set(note);
+      this.notes.set([...this.notes(), note]);
+    })
   }
 }
